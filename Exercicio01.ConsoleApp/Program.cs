9fc7d158fa0618f6eeed0c280c8ad19ee61a41cc
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-04-06 17:20:03 -0300

    codigo exercicio01

---
+++ Exercicio01.ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ecercicio01.ConsoleApp
+{
+    class Calculadora
+    {
+        public decimal comprimento;
+        public decimal largura;
+        public decimal altura;
+        public decimal v;
+
+        public void Volume()
+        {
+            Console.Write("Qual o comprimento da caixa? :");
+            comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.Write("Qual o valor da largura da caixa? :");
+            largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.Write("Qual o valor da altura? :");
+            altura = Convert.ToDecimal(Console.ReadLine());
+
+            v = comprimento * largura * altura;
+        }
+    }
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Calculadora calcular = new Calculadora();
+
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Vamos calcular o volume de uma caixa retangular");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("");
+
+            calcular.Volume();
+
+            Console.WriteLine($"o volume da caixa é {calcular.v}");
+        }
+    }
+}
