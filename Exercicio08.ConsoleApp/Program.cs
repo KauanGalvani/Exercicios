2565c47e632cdd83d93644291f37f25933611141
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-04-07 14:03:13 -0300

    adicionado exercicios

--- Exercicio08.ConsoleApp/Program.cs
+++ Exercicio08.ConsoleApp/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Exercicios.ConsoleApp
+﻿namespace Ecercicios.ConsoleApp
 {
+    class Calculadora
+    {
+        public double altura;
+        public double raio;
+        public double Valorvolume;
+        public void Volume()
+        {
+            Console.Write("Qual a altura da lata?: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Qual o raio da lata?: ");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            Valorvolume = Math.PI * Math.Pow(raio, 2) * altura;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Calculadora Calculo = new Calculadora();
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Calculo do volume de uma lata de óleo");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("");
+
+            Calculo.Volume();
+
+            Console.WriteLine($"O volume da lata de óleo é {Calculo.Valorvolume:F1}");
         }
     }
 }
