61b83b68ef0ac7694f8967f6f23df5fae9e724fa
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-04-08 11:45:58 -0300

    terminado exercicios

---
+++ Exercicio09.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exercicios.ConsoleApp
+{
+    class CalculoAluno
+    {
+        public double n1;
+        public double n2;
+        public double n3;
+        public double mediaHaromnica;
+
+        public void MediaHarmonica()
+        {
+            Console.Write("Digite a primeira nota: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a terceira nota: ");
+            double n3 = Convert.ToDouble(Console.ReadLine());
+
+            double mediaHarmonica = 3 / ((1 / n1) + (1 / n2) + (1 / n3));
+
+                Console.WriteLine($"Média harmônica: {mediaHarmonica:F2}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculoAluno alunoNota = new CalculoAluno();
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Média harmonica Alunos");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine();
+
+            alunoNota.MediaHarmonica();
+        }
+    }
+}
---
+++ Exercicio10.ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Exercicios.ConsoleApp
+{
+
+    class NotasEscola
+    {
+        public double nota1;
+        public double nota2;
+        public double peso1;
+        public double peso2;
+
+        public double Media;
+        public void MediaPonderada()
+        {
+            Console.Write("Digite a nota da primeira prova: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+
+            Console.Write("Digite o peso da primeira prova: ");
+            peso1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+
+            Console.Write("Digite a nota da segunda prova: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+
+            Console.Write("Digete o peso da segunda prova:");
+            peso2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+
+            Media = (nota1 * peso1 + nota2 * peso2) / (peso1 + peso2);
+
+            Console.WriteLine($"A média ponderada do aluno é {Media:F2}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotasEscola NotaProvas = new NotasEscola();
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Média ponderada do aluno");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("");
+
+            NotaProvas.MediaPonderada();
+        }
+    }
+}
